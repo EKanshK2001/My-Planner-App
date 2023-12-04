2c4cdd6d5af3b8e0cbfdcbbe95ca6b02eb8e492b
Ekansh <efuforofficial@gmail.com>
2023-12-04 09:35:51 +0530

    updated the create plan form alot to look better

--- scripts.js
+++ scripts.js
@@ -23,80 +23,10 @@ for (let i = 0; i < dateArea.length; i++) {
 }
 
 
-// function toggleForm() {
-//     createPlanForm.classList.toggle("hide");
-// }
-
 function togglePlusIn() {
-    plusIcon.style.color = '#e9ecef';
+    plusIcon.style.color = '#f8f9fa';
 }
 
 function togglePlusOut() {
     plusIcon.style.color = '#fd7e14';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// media query to resize add plans plus button
-
-
-
-
-// function mediaFunction(x) {
-//     if (x) { // If media query matches
-//         addPlanIcon.classList.add = "bx-lg";
-//     }
-
-// };
-
-
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-        addPlanIcon.classList.add = "bx-lg";
-    } /* else {
-        document.body.style.backgroundColor = "pink";
-    } */
-}
-
-const x = window.matchMedia("(max-width: 1024px)")
-
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
-
-
-//   var x = window.matchMedia("(max-width: 475px)")
-//   myFunction(x) // Call listener function at run time
-//   x.addListener(myFunction) // Attach listener function on state changes
-
-
-//   2xl max-width: 1536px
-//   xl max-width: 1280px
-//   lg max-width: 1024px
-//   md max-width: 768px
-//   xs max-width: 640px
-//   2xs max-width: 475px
-
-// window.innerWidth <= 1024
